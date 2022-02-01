39cc36c289230c3c01ff8027ce481f290cc8bc50
bbetanc1 <bbetancourt@ufl.edu>
2022-02-01 15:20:00 -0500

    checking sample

--- R/simulation.R
+++ R/simulation.R
@@ -18,15 +18,15 @@ SimData <- function(true_L, nfields, ncat, true_beta){
   true_M <- length(true_L)
   true_K <- sum(true_L)
   N <- sum(c(1:true_M)*true_L)#100
-  id <- rep(1:true_K,times=rep(1:true_M,times=true_L))
+  id <- rep(1:true_K, times = rep(1:true_M, times = true_L))
   ## generate true entities
-  y <- matrix(data = sample.int(n = ncat,size = true_K*nfields,replace = TRUE),
-             nrow = true_K,ncol = nfields)
-  data<-matrix(NA,nrow = N,ncol = nfields)
+  y <- matrix(data = sample.int(n = ncat, size = true_K*nfields, replace = TRUE),
+             nrow = true_K, ncol = nfields)
+  data <- matrix(NA, nrow = N, ncol = nfields)
   for (i in 1:N){
     for (l in 1:nfields){
       if(runif(1) < true_beta){
-        data[i,l] <- sample.int(n = ncat[l],size = 1)
+        data[i,l] <- sample.int(n = ncat[l], size = 1)
       }else{
         data[i,l] <- y[id[i],l]
       }
