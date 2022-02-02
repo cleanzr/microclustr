63dcdb3a305ad2881f6d869076133db796bd7081
bbetanc1 <bbetanc1@soe.ucsc.edu>
2022-02-02 09:09:58 -0500

    sample checks

--- R/error_rates.R
+++ R/error_rates.R
@@ -12,14 +12,14 @@
 #' uniqueNumberRecords <- sum(truePartition)
 #' id <- rep(1:uniqueNumberRecords, times=rep(1:maxPartitionSize, times=truePartition))
 #' fdr_fun(z = truePartition, id)
-fdr_fun <- function(z,id){
-  if(n_matches_fun(z)==0){
-    return(0)
-  }else{
-    return(
-      sum(vapply(X = c(1:length(z)), FUN = i_false_det_fun, FUN.VALUE = 1, z.=z,id.=id))/(2*n_matches_fun(z))
-    )
-  }
+fdr_fun <- function(z, id) {
+	if (n_matches_fun(z) == 0) {
+		return(0)
+	} else {
+		return(sum(vapply(X = c(1:length(z)), FUN = i_false_det_fun, 
+			FUN.VALUE = 1, z. = z, id. = id))/(2 * 
+			n_matches_fun(z)))
+	}
 }
 
 #' Calculates FNR when ground truth is available
@@ -34,14 +34,14 @@ fdr_fun <- function(z,id){
 #' uniqueNumberRecords <- sum(truePartition)
 #' id <- rep(1:uniqueNumberRecords, times=rep(1:maxPartitionSize, times=truePartition))
 #' fnr_fun(z = truePartition, id)
-fnr_fun <- function(z,id){
-  if(n_matches_fun(id)==0){
-    return(0)
-  }else{
-    return(
-      sum(vapply(X = c(1:length(z)),FUN = i_false_neg_fun,FUN.VALUE = 1,z.=z,id.=id))/(2*n_matches_fun(id))
-    )
-  }
+fnr_fun <- function(z, id) {
+	if (n_matches_fun(id) == 0) {
+		return(0)
+	} else {
+		return(sum(vapply(X = c(1:length(z)), FUN = i_false_neg_fun, 
+			FUN.VALUE = 1, z. = z, id. = id))/(2 * 
+			n_matches_fun(id)))
+	}
 }
 
 #' Calculates average FDR when ground truth is available
@@ -61,9 +61,10 @@ fnr_fun <- function(z,id){
 #' simulatedData <- SimData(truePartition, numberFields, numberCategories, trueBeta)
 #' posteriorESCD <- SampleCluster(data=simulatedData, Prior="ESCD", burn=0, nsamples=10)
 #' mean_fdr(zm = posteriorESCD$Z, id)
-mean_fdr <- function(zm,id){
-  fdr_vec <- apply(X = zm, MARGIN = 1, FUN = fdr_fun, id=id)
-  return(mean(fdr_vec))
+mean_fdr <- function(zm, id) {
+	fdr_vec <- apply(X = zm, MARGIN = 1, FUN = fdr_fun, 
+		id = id)
+	return(mean(fdr_vec))
 }
 
 #' Calculates average FNR when ground truth is available
@@ -83,31 +84,34 @@ mean_fdr <- function(zm,id){
 #' simulatedData <- SimData(truePartition, numberFields, numberCategories, trueBeta)
 #' posteriorESCD <- SampleCluster(data=simulatedData, Prior="ESCD", burn=0, nsamples=10)
 #' mean_fnr(zm = posteriorESCD$Z, id)
-mean_fnr <- function(zm,id){
-  fnr_vec <- apply(X = zm, MARGIN = 1, FUN = fnr_fun, id=id)
-  return(mean(fnr_vec))
+mean_fnr <- function(zm, id) {
+	fnr_vec <- apply(X = zm, MARGIN = 1, FUN = fnr_fun, 
+		id = id)
+	return(mean(fnr_vec))
 }
 
 # auxiliary functions to compute fnr and fdr
 
-i_false_det_fun <- function(i,z.,id.){
-  i_match <- which(z.[-i]==z.[i])
-  i_match_true <- which(id.[-i]==id.[i])
-  i_common <- length(intersect(i_match,i_match_true))
-  i_false_det <- length(i_match)-i_common
-  return(i_false_det)
+i_false_det_fun <- function(i, z., id.) {
+	i_match <- which(z.[-i] == z.[i])
+	i_match_true <- which(id.[-i] == id.[i])
+	i_common <- length(intersect(i_match, i_match_true))
+	i_false_det <- length(i_match) - i_common
+	return(i_false_det)
 }
 
-i_false_neg_fun <- function(i,z.,id.){
-  i_match <- which(z.[-i]==z.[i])
-  i_match_true <- which(id.[-i]==id.[i])
-  i_common <- length(intersect(i_match,i_match_true))
-  i_false_neg <- length(i_match_true)-i_common
-  return(i_false_neg)
+i_false_neg_fun <- function(i, z., id.) {
+	i_match <- which(z.[-i] == z.[i])
+	i_match_true <- which(id.[-i] == id.[i])
+	i_common <- length(intersect(i_match, i_match_true))
+	i_false_neg <- length(i_match_true) - i_common
+	return(i_false_neg)
 }
 
-n_matches_fun <- function(z){
-  sizes <- table(z)
-  n_match <- sum(vapply(X = sizes,FUN = function(s){return(choose(s,2))},FUN.VALUE = 1))
-  return(sum(n_match))
+n_matches_fun <- function(z) {
+	sizes <- table(z)
+	n_match <- sum(vapply(X = sizes, FUN = function(s) {
+		return(choose(s, 2))
+	}, FUN.VALUE = 1))
+	return(sum(n_match))
 }
--- R/simulation.R
+++ R/simulation.R
@@ -14,26 +14,28 @@
 #' numberCategories <- rep(5,2)
 #' trueBeta <- 0.01
 #' SimData(truePartition, numberFields, numberCategories, trueBeta)
-SimData <- function(true_L, nfields, ncat, true_beta){
-  true_M <- length(true_L)
-  true_K <- sum(true_L)
-  N <- sum(c(1:true_M)*true_L)#100
-  id <- rep(1:true_K, times = rep(1:true_M, times = true_L))
-  ## generate true entities
-  y <- cbind()
-  for (l in 1:nfields){
-     y <- cbind(y, sample.int(n = ncat[l], size = true_K, replace = TRUE))
-  }  
-  data <- matrix(NA, nrow = N, ncol = nfields)
-  for (i in 1:N){
-    for (l in 1:nfields){
-      if(runif(1) < true_beta){
-        data[i,l] <- sample.int(n = ncat[l], size = 1)
-      }else{
-        data[i,l] <- y[id[i],l]
-      }
-    }
-  }
-  x <- DataRemap(data)
-  return(x)
+SimData <- function(true_L, nfields, ncat, true_beta) {
+	true_M <- length(true_L)
+	true_K <- sum(true_L)
+	N <- sum(c(1:true_M) * true_L) #100
+	id <- rep(1:true_K, times = rep(1:true_M, times = true_L))
+	## generate true entities
+	y <- cbind()
+	for (l in 1:nfields) {
+		y <- cbind(y, sample.int(n = ncat[l], size = true_K, 
+			replace = TRUE))
+	}
+	data <- matrix(NA, nrow = N, ncol = nfields)
+	for (i in 1:N) {
+		for (l in 1:nfields) {
+			if (runif(1) < true_beta) {
+				data[i, l] <- sample.int(n = ncat[l], 
+					size = 1)
+			} else {
+				data[i, l] <- y[id[i], l]
+			}
+		}
+	}
+	x <- DataRemap(data)
+	return(x)
 }
