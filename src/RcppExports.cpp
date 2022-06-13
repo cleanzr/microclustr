246ed21b585be26a800806bb58010afd2ff11686
Changwoo Lee <c.lee@stat.tamu.edu>
2022-06-13 12:54:20 +0900

    autogenerated files by package compiling

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // loglikxSP
 double loglikxSP(NumericVector betas, IntegerMatrix x, IntegerVector z, List params);
 RcppExport SEXP _microclustr_loglikxSP(SEXP betasSEXP, SEXP xSEXP, SEXP zSEXP, SEXP paramsSEXP) {
